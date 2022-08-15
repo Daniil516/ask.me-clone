1136ae57209252847bf8545c96a78c0640c69bbf
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-08-15 12:10:15 +0300

    homework-changes-15.08.2022-12:10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   before_validation :downcase_nickname
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
-  validates :nickname, uniqueness: true, length: { maximum: 40 }, format: {with: /[a-zA-Z0-9_]/}
+  validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: /[a-zA-Z0-9_]/ }
+  validates :header_color, presence: true, format: { with: /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/ }
 
   def downcase_nickname
     nickname.downcase!
