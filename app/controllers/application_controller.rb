f2915204246d5b89397c2e4e4ba1dd8609fd4ebf
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-08-17 17:29:32 +0300

    add safe authorizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+
+  private
+
+  def current_user #Если переменная класса с пользователем не инициализирована и в сессии есть user_id,
+    # то есть пользователь авторизован, тогда выполняем запрос к бд и ищем нашего юзера
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def redirect_with_alert
+    redirect_to root_path, alert: "You don't have the rights to do this"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :authorize_user, only: %i[edit update destroy]
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: "You have successfully registered on AskMe"
@@ -16,12 +19,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #just gives form for update
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to root_path, notice: "You have successfully edited your data on AskMe"
     else
@@ -39,12 +40,19 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @questions = @user.questions
     @question = Question.new(user_id: @user.id)
   end
 
   private
+
+  def authorize_user
+    redirect_with_alert unless @user == current_user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation, :header_color)
