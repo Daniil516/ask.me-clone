78183d4f0d500fc96e03f2c9bf9d05e561023c0f
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-08-17 13:15:39 +0300

    header_color-fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
   validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: /[a-zA-Z0-9_]/ }
-  validates :header_color, presence: true, format: { with: /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/ }
+  validates :header_color, format: { with: /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/ }
 
   def downcase_nickname
     nickname.downcase!
