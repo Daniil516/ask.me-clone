b20a826e15d7f1480c254133a8fdd598f3193ac8
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-08-29 02:37:49 +0300

    refactoring

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,31 +1,24 @@
 class Question < ApplicationRecord
-  belongs_to :user
-  belongs_to :author, class_name: "User", optional: true
-
-  has_many :question_hashtags
-  has_many :hashtags, through: :question_hashtags
-
   validates :body, presence: true, length: { maximum: 280 }
-
   before_validation :find_hashtags
   after_commit :save_hashtags
 
+  belongs_to :user
+  belongs_to :author, class_name: "User", optional: true
+
+  has_many :question_hashtags, dependent: :destroy
+  has_many :hashtags, through: :question_hashtags
+
   def find_hashtags
-    @hashtags = body.scan(/#[[:word:]-]+/)
-    @hashtags += answer.scan(/#[[:word:]-]+/) if answer.present?
-    @hashtags.map! { |elem| elem.downcase }
+    hashtag_pattern = /#[[:word:]-]+/
+    @hashtags = (body + answer.to_s).downcase.scan(hashtag_pattern)
   end
 
   def save_hashtags
-    #Чистим старые хэштеги вопроса на случай апдейта вопроса
-    self.hashtags.destroy_all if self.persisted?
-
-    #Если хэштег был использован кем то ранее в других вопросах,
-    # то нам незачем создавать его повторно. Просто инициализируем его для конкретного вопроса, а не в общем
-    @hashtags.uniq.each do |hashtag|
+    self.hashtags.destroy_all
+    
+    @hashtags&.uniq&.each do |hashtag|
       if Hashtag.where(body: hashtag).present?
-        debugger
-        #self.hashtags.where(body: hashtag)[0].question_hashtags.create
         Hashtag.where(body: hashtag)[0].question_hashtags.create(question_id: id)
       else
         self.hashtags.create(body: hashtag)
