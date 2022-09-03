57078e45813216496f3223af0a9767decd76aef0
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-09-03 13:29:31 +0300

    404 experimental

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,7 +2,11 @@ class HashtagsController < ApplicationController
 
   def show
     @hashtag = Hashtag.find_by(body: "##{params[:body]}")
-    @questions = @hashtag.questions
+    @questions = @hashtag&.questions or not_found
+  end
+
+  def not_found
+    raise ActiveRecord::RecordNotFound.new('Record is not found')
   end
 
   def to_param
